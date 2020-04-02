2b6ed740e50641d4af3fbbf81acd12798738de56
tomas scheto <schetin_2@hotmail.com.ar>
2020-04-01 22:26:06 -0300

    Prueba 7u7

---
+++ HolaMundoST/HolaMundoST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolaMundoST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
